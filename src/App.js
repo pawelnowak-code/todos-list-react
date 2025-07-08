8c77b68a8d778c83e19442c0ff54973224c9ac1b
Paweł Nowak <hitec00@interia.pl>
2025-07-08 11:49:24 +0200

    tasks list without interactions yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from './Form';
+import Tasks from './Tasks';
+import Buttons from './Buttons';
+import Section from './Section';
+import Container from './Container';
+import Header from './Header';
+
+const tasks = [
+  { id: 1, content: "Wykopać głęboki dół", done: true },
+  { id: 2, content: "Polecieć na księżyć", done: false }
+];
+
+const hideDoneTasks = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+      <Section
+        title="Lista zadań"
+        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+      />
+
+    </Container>
   );
 }
 
